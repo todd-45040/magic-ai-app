e8482a2438581113474b2e439762a9d7c3fbd9c6
todd-45040 <toddssmartphone@gmail.com>
2026-04-02 13:16:44 -0400

    Soft Release at ADMC version v1.0.0 on April 2nd, 2026. Let's Go..!!

--- constants.ts
+++ constants.ts
@@ -31,7 +31,7 @@ import {
 
 export const ADMIN_EMAIL = 'admin@magicaiwizard.com';
 // Keep this in sync with your current git benchmark/tag so the UI + SW cache are coherent.
-export const APP_VERSION = 'v0.99.9-beta';
+export const APP_VERSION = 'v1.0.0';
 
 // Add GUEST_USER for Audience Mode services
 export const GUEST_USER: User = {
